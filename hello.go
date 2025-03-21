aeb00d476abee3166b53b944400ae6aa4def8007
pittalaabhishek <pittalaabhishek999@gmail.com>
2025-03-21 14:50:57 +0530

    initial commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Alice"
+	age := 30
+	fmt.Printf("Name: %s, Age: %d\n", name, age)
+	fmt.Println("Hello, World!")
+	fmt.Printf("hello dude\n")
+}
